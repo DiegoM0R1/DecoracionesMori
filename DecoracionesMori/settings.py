35dbc7d3c648a5cef6a31e616b3d9351e410e763
DiegoM0R1 <zuluetamori@gmail.com>
2025-05-18 05:12:23 -0500

    instalacion de requerimentos para render

--- DecoracionesMori/settings.py
+++ DecoracionesMori/settings.py
@@ -317,6 +317,7 @@ WSGI_APPLICATION = 'DecoracionesMori.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
