282318289fe1ab454f46243a7f2dbbd30d52e1c1
DiegoM0R1 <zuluetamori@gmail.com>
2025-05-18 05:04:17 -0500

    implementaciones de cotizaciones antes de solicitar servicio para render

--- DecoracionesMori/settings.py
+++ DecoracionesMori/settings.py
@@ -319,14 +319,11 @@ WSGI_APPLICATION = 'DecoracionesMori.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'decoraciones_mori',
-        'USER': 'root',
-        'PASSWORD': 'D1alZuM0',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Asegúrate de que BASE_DIR esté definido
     }
 }
+
 
 
 # Password validation
