6b6449de85ed8d237960cef891559ca4a3aec7e6
DiegoM0R1 <zuluetamori@gmail.com>
2025-10-14 07:01:05 -0500

    actualizacion de cambios de estilos

--- DecoracionesMori/settings.py
+++ DecoracionesMori/settings.py
@@ -313,7 +313,7 @@ WSGI_APPLICATION = 'DecoracionesMori.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -331,7 +331,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
